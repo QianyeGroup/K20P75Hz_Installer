b7ade7fd7efdf75671b9a097d71744dcab446f21
真心 <ceo@iqianye.cn>
2019-07-26 13:53:23 +0800

    添加更新检测

---
+++ app/src/main/java/cn/iqianye/miui/k20p/screen/utils/UpdateUtils.java
@@ -0,0 +1,14 @@
+package cn.iqianye.miui.k20p.screen.utils;
+
+public class UpdateUtils
+{
+	long latestVersion;
+	public static String getNewVersionInfo(String version)
+	{
+		
+	}
+	public static Boolean checkVersion(String version)
+	{
+		
+	}
+}
